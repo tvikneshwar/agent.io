ba78cb48ead65ea653dbdcf11b2092f349b7df2d
Vikneshwar Thandeswaran <tvikneshwar@gmail.com>
2021-04-15 06:54:34 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+//listen for submit event//
+document.getElementById('registrationform').addEventListener('submit', formSubmit);
+
+//Submit form
+function formSubmit(e) {
+  e.preventDefault();
+  // Get Values from the DOM
+  let name = document.querySelector('#name').value;
+  let email = document.querySelector('#email').value;
+  let password = document.querySelector('#password').value;
+  let bio = document.querySelector('#bio').value;
+  let job = document.querySelector('#job').value;
+  let interest = document.querySelector('#interest').value;

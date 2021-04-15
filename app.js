ddfe514655453d858901ead08876b55641104255
Vikneshwar Thandeswaran <tvikneshwar@gmail.com>
2021-04-15 07:15:53 +0000

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ function formSubmit(e) {
   let bio = document.querySelector('#bio').value;
   let job = document.querySelector('#job').value;
   let interest = document.querySelector('#interest').value;
+
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBda-9bZjeQBBg-jYeFb2rcuKKrcm1NIEU",
+    authDomain: "bairavi-83fd2.firebaseapp.com",
+    projectId: "bairavi-83fd2",
+    storageBucket: "bairavi-83fd2.appspot.com",
+    messagingSenderId: "482331018891",
+    appId: "1:482331018891:web:1bfdb886d72b293a2c768e"
+  };

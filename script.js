f9ea694a058f751dd8fb6a961774c106180148fd
Vikneshwar Thandeswaran <tvikneshwar@gmail.com>
2021-06-19 20:16:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 /*
    Eclipse Paho MQTT-JS Utility
-   by Elliot Williams for Hackaday article, 
+   by Elliot Williams for Hackaday article,
 */
 // Global variables
 
@@ -9,7 +9,7 @@
 var MyApp = {}; // Globally scoped object
 var client       = null;
 var led_is_on    = null;
-// These are configs	
+// These are configs
 //var hostname       = "broker.hivemq.com";
 //var port           = "8000";
 var hostname       = "broker.emqx.io";
@@ -24,10 +24,13 @@ var topic1         = "home/outdoors/tp1";
 
 // This is called after the webpage is completely loaded
 // It is the main entry point into the JS code
-
-
-
-
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+console.log ( dateTime);
+var today = new Date();
+console.log ( today);
 
 function calculateTotal()
 {
@@ -35,21 +38,21 @@ function calculateTotal()
     saltfull: 50,
     saltpowder: 12,
     mustard: 95,
-    venthyam:99, 
+    venthyam:99,
      geeraham:191
   };
   let item_price={}
-  
+
 
   areabeat  = ($("#beat").val());
   shop_name = ($("#shop").val());
   phone_num = ($("#phone").val());
-  
+
   item_price.saltfull = ($("#qty_saltfull").val() * unit_price.saltfull )
   $("#price_saltfull").val(item_price.saltfull);
   saltfull_QTY= ($("#qty_saltfull").val());
 
-  
+
   item_price.saltpowder = ($("#qty_saltpowder").val() * unit_price.saltpowder )
   $("#price_saltpowder").val(item_price.saltpowder);
    saltpowder_QTY= ($("#qty_saltpowder").val());
@@ -57,39 +60,46 @@ function calculateTotal()
 
 
   item_price.mustard = ($("#qty_mustard").val() * unit_price.mustard )
-  $("#price_mustard").val(item_price.mustard);  
+  $("#price_mustard").val(item_price.mustard);
   mustard_QTY= ($("#qty_mustard").val());
 
 
   item_price.venthyam = ($("#qty_venthyam").val() * unit_price.venthyam )
-  $("#price_venthyam").val(item_price.venthyam);    
+  $("#price_venthyam").val(item_price.venthyam);
   venthyam_QTY= ($("#qty_venthyam").val());
 
 item_price.geeraham = ($("#qty_geeraham").val() * unit_price.geeraham )
-  $("#price_geeraham").val(item_price.geeraham);    
+  $("#price_geeraham").val(item_price.geeraham);
   geeraham_QTY= ($("#qty_geeraham").val());
-	
-	
- 
+
+
+
   let total = item_price.saltfull + item_price.saltpowder + item_price.mustard + item_price.venthyam + item_price.geeraham;
 
- 
+
   $("#total_value").text(total);
 
 //###########################################################################################################
 
-var jsonObject =[{Beat: areabeat, shop: shop_name,Phone: phone_num,sugar: saltfull_QTY,butter: saltpowder_QTY,Egg: mustard_QTY,vanilla: venthyam_QTY}]
+//var jsonObject =[{Beat: areabeat, shop: shop_name,Phone: phone_num,sugar: saltfull_QTY,butter: saltpowder_QTY,Egg: mustard_QTY,vanilla: venthyam_QTY}]
+var jsonObject = {"Beat":areabeat, "shop":shop_name, "Phone":phone_num ,"Date":dateTime,"sugar":saltfull_QTY , "Saltpowder":saltpowder_QTY , "Mustard": mustard_QTY , "VENTHYAM": venthyam_QTY };
 //var data={"BEAT":"mm","SHOP":"kk","SALT-QTY":"2","MUSTARD-QTY":"2","GEERA-QTY":"2","VENTHYAM-QTY":"3"}
-var data= JSON.stringify(jsonObject);
+//var data= JSON.stringify(jsonObject);
+var data=jsonObject;
 MyApp.color = data;
 //bucket = json;
 console.log(data);
 //console.log(bucket);
-    
+
 }
 //############################################################################################################
 
-
+function refreshPage(){
+		if(confirm("Are you sure, want to place order"))
+    {
+			location.reload();
+		}
+	}
 
 
 $(function()
@@ -101,7 +111,7 @@ $(function()
 function connect(){
 	// Set up the client
 	client = new Paho.MQTT.Client(hostname, Number(port), clientId);
-	console.info('Connecting to Server: Hostname: ', hostname, 
+	console.info('Connecting to Server: Hostname: ', hostname,
 			'. Port: ', port, '. Client ID: ', clientId);
 
 	// set callback handlers
@@ -146,7 +156,7 @@ function onMessageArrived(message) {
 	console.log(message.destinationName, message.payloadString);
 
 	// Update element depending on which topic's data came in
-	if (message.destinationName == temp_topic){ 
+	if (message.destinationName == temp_topic){
 		var temperature_heading = document.getElementById("temp_display");
 		temperature_heading.innerHTML = "Temperature: " + message.payloadString + " &deg;C";
 	} else if (message.destinationName == humidity_topic) {
@@ -166,7 +176,7 @@ function onMessageArrived(message) {
 
 
 function duck(){
-	
+
 
      //alert(MyApp.color);
 
@@ -192,4 +202,3 @@ function duck(){
 		console.info('sending: ', payload);
 
     	  }
-    	
